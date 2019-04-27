f00bf3c8bfed9f1c8c11d4b02878a5c08cb4b883
Melissa LeBlanc-Williams <melissa@Melissas-MacBook-Pro.local>
2019-04-26 20:07:37 -0700

    Simplified example

--- examples/hx8357_simpletest.py
+++ examples/hx8357_simpletest.py
@@ -1,7 +1,7 @@
 """
-This test will initialize the display using displayio
-and draw a solid red background
-"""
+  This test will initialize the display using displayio
+  and draw a solid red background
+  """
 
 import board
 import displayio
@@ -24,14 +24,9 @@ color_bitmap = displayio.Bitmap(480, 320, 1)
 color_palette = displayio.Palette(1)
 color_palette[0] = 0xFF0000
 
-try:
-    bg_sprite = displayio.TileGrid(color_bitmap,
-                                   pixel_shader=color_palette,
-                                   position=(0, 0))
-except TypeError:
-    bg_sprite = displayio.TileGrid(color_bitmap,
-                                   pixel_shader=color_palette,
-                                   x=0, y=0)
+bg_sprite = displayio.TileGrid(color_bitmap,
+                               pixel_shader=color_palette,
+                               x=0, y=0)
 splash.append(bg_sprite)
 
 while True:
