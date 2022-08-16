d0b2cf73d3192c66e82b821def362113507695b6
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:13 -0400

    Update version string

--- adafruit_hx8357.py
+++ adafruit_hx8357.py
@@ -32,7 +32,7 @@ Implementation Notes
 
 import displayio
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_HX8357.git"
 
 _INIT_SEQUENCE = (
